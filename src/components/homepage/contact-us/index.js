0b1e1a0798cb963ae6b01d081ff78871887f7da6
gitcarl <carl@torchlighttechnology.com>
2019-11-10 16:45:33 -0500

    email submit

--- src/components/homepage/contact-us/index.js
+++ src/components/homepage/contact-us/index.js
@@ -1,26 +1,76 @@
-import React from 'react'
+import React from "react"
+import { Form, Formik, ErrorMessage, Field } from "formik"
+import axios from "axios"
 
 const ContactUs = () => {
   return (
     <section className="section contact-us-form">
       <div className="container is-widescreen">
-        <form>
-          <h3>Get the best of Petplace straight to your inbox.</h3> 
-          <div className="columns">
-            <div className="column">
-              <input type="checkbox"  name="dog-newsletter"  />
-              <label htmlFor="dog-newsletter">Yes, Send Me The Dog Crazy Newsletter.</label>
-              <input type="checkbox"  name="puppy-newsletter" checked />
-              <label htmlFor="puppy-newsletter">Yes, Send Me The Puppy Newsletter.</label>
-              <input type="checkbox"  name="cat-newsletter" checked />
-              <label htmlFor="cat-newsletter">Yes, Send Me The Cat Crazy Newsletter.</label>
-            </div>
-          </div>
-          <input type="text" name="name" />
-          <input type="text" name="name" />
-          <button type="submit">Sign Up Now</button>
-        </form>
-        <p className="disclaimer">By signing up you agree to our Terms of Service and Privacy Policy</p>
+        <Formik
+          initialValues={{ email: "", email_confirm: "", dog_crazy: true, puppy: true, cat_crazy: true}}
+          validate={values => {
+            let errors = {}
+            if(!values.email){
+              errors.email = 'Email is Required'
+            }
+            if (values.email !== values.email_confirm) {
+              errors.email = "Email values don't match"
+            }
+            return errors;
+          }}
+          onSubmit={values => {
+            axios
+              .post(
+                "https://psluo758l6.execute-api.us-east-1.amazonaws.com/prod/emails",
+                {
+                  email: values.email,
+                  puppy: values.puppy,
+                  dog_crazy: values.dog_crazy,
+                  cat_crazy: values.cat_crazy,
+                  date: new Date().toUTCString()
+                }
+              )
+
+            console.log(values)
+          }}
+          render={({ values, errors }) => (
+            <Form>
+              <h3>Get the best of PetPlace straight to your inbox.</h3>
+              <ErrorMessage name="email" component="div" />
+              <div className="columns">
+                <div className="column">
+                  <Field
+                    type="checkbox"
+                    name="dog_crazy"
+                    checked={values.dog_crazy}
+                  />
+                  <label htmlFor="dog_crazy">
+                    Yes, Send Me The Dog Crazy Newsletter.
+                  </label>
+                  <Field
+                    type="checkbox"
+                    name="puppy"
+                    checked={values.puppy}
+                  />
+                  <label htmlFor="puppy">
+                    Yes, Send Me The Puppy Newsletter.
+                  </label>
+                  <Field
+                    type="checkbox"
+                    name="cat_crazy"
+                    checked={values.cat_crazy}
+                  />
+                  <label htmlFor="cat_crazy">
+                    Yes, Send Me The Cat Crazy Newsletter.
+                  </label>
+                </div>
+              </div>
+              <Field type="email" name="email"  />
+              <Field type="email" name="email_confirm"  />
+              <button type="submit">Sign Up Now</button>
+            </Form>
+          )}
+        />
       </div>
     </section>
   )
