0030d6a52e300e0466462901c750450c8deabbe7
Slobodan Perichin <perichin@gmail.com>
2019-11-17 22:03:55 +0100

    breadcrumbs working

--- src/components/Breadcrumbs.js
+++ src/components/Breadcrumbs.js
@@ -4,13 +4,28 @@ import { categoryColor } from '../components/functions';
 import homeIcon from '../images/home_bread_crumb.svg';
 import rightArrow from '../images/right-arrow-bc.svg';
 
-export default (props) => (
+export default (props) => {
+
+  const name = props.post.categories && props.post.categories[0] && props.post.categories[0].name;
+  const path = props.post.categories && props.post.categories[0] && props.post.categories[0].path;
+  const categories = path.split('/').filter((cat) => cat && cat !== 'article' && cat !== 'category');
+
+  return (
     <div className="breadcrumbs">
-        <Link to="/" className="home"><img src={homeIcon} alt="home" /></Link>
-        <Link to="/" className={`category-link ${categoryColor(props.post.categories && props.post.categories[0] && props.post.categories[0].name)}`}>Dog Care</Link>
-        <div className="divider"><img src={rightArrow} alt="right arrow"/></div>
-        <Link to="/" className={`category-link ${categoryColor(props.post.categories && props.post.categories[0] && props.post.categories[0].name)}`}>Dog Exercise &amp; Play</Link>
-        <div className="divider"><img src={rightArrow} alt="right arrow"/></div>
-        <div className={`category-link ${categoryColor(props.post.categories && props.post.categories[0] && props.post.categories[0].name)}`}>Puppy Play</div>
+      <Link to="/" className="home"><img src={homeIcon} alt="home" /></Link>
+      {
+        categories.map(((cat, i) => {
+          const categoryPath = path.split(cat)[0] + `${cat}/`;
+          return (
+            <>
+              <Link to={categoryPath} className={`category-link ${categoryColor(name)}`}>{cat.replace(/-/g, ' ')}</Link>
+              {
+                i < categories.length - 1 ? <div className="divider"><img src={rightArrow} alt="right arrow"/></div> : null
+              }
+            </>
+          )
+        }))
+      }
     </div>
-);
+  )
+}
