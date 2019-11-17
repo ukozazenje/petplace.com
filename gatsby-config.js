675c55ab44161fea7340ca47a17d3e38f24ebdaf
gitcarl <carl@torchlighttechnology.com>
2019-11-17 12:04:08 -0500

    add gatsby gtm code

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,6 +36,26 @@ module.exports = {
       },
     },
     {
+      resolve: "gatsby-plugin-google-tagmanager",
+      options: {
+        id: "GTM-5V3N739",
+
+        // Include GTM in development.
+        // Defaults to false meaning GTM will only be loaded in production.
+        includeInDevelopment: true,
+
+        // datalayer to be set before GTM is loaded
+        // should be an object or a function that is executed in the browser
+        // Defaults to null
+        defaultDataLayer: { platform: "gatsby" },
+
+        // Specify optional GTM environment details.
+        // gtmAuth: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_AUTH_STRING",
+        // gtmPreview: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_PREVIEW_NAME",
+        // dataLayerName: "YOUR_DATA_LAYER_NAME",
+      },
+    },
+    {
       resolve: "gatsby-source-wordpress",
       options: {
         /*
@@ -133,7 +153,7 @@ module.exports = {
         },
       },
     },
-    // { 
+    // {
     //   resolve: `gatsby-plugin-purgecss`,
     //   options: {
     //     printRejected: true, // Print removed selectors and processed file names
