0ae3ef50a590d1b021f5d6f138692f3db97379ba
kyle feeley <me@kylepfeeley.com>
2019-11-19 16:20:52 -0500

    Change class name to remove "ad"

--- src/components/AdSet.js
+++ src/components/AdSet.js
@@ -4,7 +4,7 @@ import smallPPIImg from '../images/ads/PPI-250x250-11152019.jpg';
 
 const AdSet = (props) => (
     <a href={`https://www.petpartners.com/pet-insurance/how-it-works?cs=PETPLACE&csd=${props.title}`} 
-        target="_blank" rel="noopener noreferrer" className="adset-wrapper">
+        target="_blank" rel="noopener noreferrer" className="ppiimg-wrapper">
         <picture>
             <source srcSet={smallPPIImg} media="(max-width: 768px)"></source>
             <source srcSet={tallPPIImg} media="(min-width: 769px)"></source>
