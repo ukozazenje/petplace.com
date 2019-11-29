78a32983f7361f86f4692ebe0dd1394e046651a6
ukozazenje <mzecevic04@gmail.com>
2019-11-30 00:05:39 +0100

    Fixing images loading indicator for home search btn

--- src/components/Search.js
+++ src/components/Search.js
@@ -190,7 +190,7 @@ export default class Search extends Component {
                               post && 
                               post.featured_image &&  
                               post.featured_image ? 
-                              <img src={post.featured_image} alt="" /> :
+                              <img src={post.featured_image.replace(process.env.GATSBY_WP_URL, process.env.GATSBY_PP_URL)} alt="" /> :
                               <NoImg />
                             }
                           </Link>
