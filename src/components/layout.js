6e593ebd70a7f085763ef59d9a205af2e82bca2b
kyle feeley <me@kylepfeeley.com>
2019-12-13 11:10:25 -0500

    Update layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,22 +1,63 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
 import React from "react"
 import PropTypes from "prop-types"
-
 import Header from "./header"
 import Footer from './footer'
+import exitIntent from 'exit-intent'
 import "../sass/main.sass"
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
 
 const Layout = ({ children, noFooter, noSearch }) => {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+  
+  const {removeExitIntent} = exitIntent({
+    threshold: 5,
+    maxDisplays: 1,
+    eventThrottle: 100,
+    onExitIntent: () => {  
+      console.log('1');
+    }    
+  })
+
   return (
     <>
       <Header noSearch={noSearch} />
+      {removeExitIntent}
       {children}
+      <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
+        <DialogTitle id="form-dialog-title">Subscribe</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            To subscribe to this website, please enter your email address here. We will send updates
+            occasionally.
+          </DialogContentText>
+          <TextField
+            autoFocus
+            margin="dense"
+            id="name"
+            label="Email Address"
+            type="email"
+            fullWidth
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary">
+            Cancel
+          </Button>
+          <Button onClick={handleClose} color="primary">
+            Subscribe
+          </Button>
+        </DialogActions>
+      </Dialog>
       {
         noFooter
         ? null :
