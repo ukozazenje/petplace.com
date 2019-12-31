d5ae1ab9408b047ce8c92d819274b05eb9dc4637
kyle feeley <me@kylepfeeley.com>
2019-12-31 01:39:02 -0500

    Fix SSR error

--- src/components/EmailDialog.js
+++ src/components/EmailDialog.js
@@ -1,19 +1,20 @@
 import React, { useState }  from 'react'
-import ExitIntent from '../components/homepage/ExitIntent'
+import exitIntent from 'exit-intent'
 import Dialog from '@material-ui/core/Dialog'
 import ContactUsSection from "../components/homepage/contact-us"
 
 export default function EmailDialog() {
-    const [count, setCount] = useState(0);
-    const [open, setOpen] = React.useState(false);
+  const [count, setCount] = useState(0);
+  const [open, setOpen] = React.useState(false);
 
+  if (typeof window !== 'undefined') {
     const handleClose = () => {
       setOpen(false);
       setCount(count + 1);
       localStorage.setItem('closed', (count + 1));  
     };
     
-    const {removeExitIntent} = ExitIntent({
+    const {removeExitIntent} = exitIntent({
       threshold: 5,
       maxDisplays: 1,
       eventThrottle: 20,
@@ -33,5 +34,8 @@ export default function EmailDialog() {
                 ) : null 
             }  
         </div>
-    )
+    ) 
+  } else {
+    return null;
+  }
 }
