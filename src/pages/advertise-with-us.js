6c92d00da7938ff38b1848d97aa513abbae18536
ukozazenje <mzecevic04@gmail.com>
2020-05-27 17:08:36 +0200

    Saving progress

--- src/pages/advertise-with-us.js
+++ src/pages/advertise-with-us.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Layout from '../components/layout'
 import HeroImage from '../static/images/advertiseHero'
 import ContentPublishingImg from '../images/mob_content_publishing.png' 
+import { Formik, Field } from 'formik'
 const advertise = () => {
   return (
     <Layout>
@@ -30,6 +31,49 @@ const advertise = () => {
               <p>Sponsor content for our site and lend your expertise to an article that will be seen by nearly 1 million people in your target demographic.</p>
             </div>
           </div>
+          <div className="advertise-box-wrapper content-publishing-img">
+            <div className="advertise-box">
+              <h3>Content Publishing</h3>
+              <p>Sponsor content for our site and lend your expertise to an article that will be seen by nearly 1 million people in your target demographic.</p>
+            </div>
+          </div>
+          <div className="advertise-box-wrapper content-publishing-img">
+            <div className="advertise-box">
+              <h3>Content Publishing</h3>
+              <p>Sponsor content for our site and lend your expertise to an article that will be seen by nearly 1 million people in your target demographic.</p>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="advertise-contact">
+        <div className="container is-fullhd">
+          <h4>Contact Us</h4>
+          <p>Fill out the form below if you’re interested in becoming a sponsor or advertiser with PetPlace.</p>
+          <Formik
+            initialValues={{ email: '', name: '', company: '', serviceOfInterest: '' }}
+            onSubmit={(values, { setSubmitting }) => {
+              setTimeout(() => {
+                alert(JSON.stringify(values, null, 2));
+                setSubmitting(false);
+              }, 400);
+            }}
+          >
+            {({
+              values,
+              errors,
+              touched,
+              handleChange,
+              handleBlur,
+              handleSubmit,
+              isSubmitting,
+              /* and other goodies */
+            }) => (
+              <Form>
+                <Field name='email' type='email' />
+                <Field name='name' type='text' />
+              </Form>
+            )}
+          </Formik>
         </div>
       </section>
     </Layout>
